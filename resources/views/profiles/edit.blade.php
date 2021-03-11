031cc195139b6302f586b996740c2ddcd23538b3
kobiyoyo <adamachubi@gmail.com>
2021-03-11 07:12:04 +0100

    add profile functionality

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -12,16 +12,17 @@
             </div>
             <div class="form-group row">
                 <label for="title" class="col-md-4 col-form-label">Title</label>
-                <input id="title" type="title" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title">
+                <input id="title" type="title" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') ?? $user->profile->title }}" required autocomplete="title">
                 @error('title')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
             </div>
+
             <div class="form-group row">
                 <label for="description" class="col-md-4 col-form-label">Description</label>
-                <input id="description" type="description" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" required autocomplete="description">
+                <input id="description" type="description" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') ?? $user->profile->description }}" required autocomplete="description">
                 @error('description')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
@@ -30,15 +31,16 @@
             </div>
             <div class="form-group row">
                 <label for="url" class="col-md-4 col-form-label">Url</label>
-                <input id="url" type="url" class="form-control @error('url') is-invalid @enderror" name="url" value="{{ old('url') }}" required autocomplete="url">
+                <input id="url" type="url" class="form-control @error('url') is-invalid @enderror" name="url" value="{{ old('url') ?? $user->profile->url }}" required autocomplete="url">
                 @error('url')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                 @enderror
             </div>
+
             <div>
-                <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                <label for="image" class="col-md-4 col-form-label">Profile Image</label>
 
                 <input type="file" class="form-control-file" id="image" name="image">
                 @error('image')
@@ -48,7 +50,7 @@
                 @enderror
             </div>
             <div class="row pt-4">
-                <button class="btn btn-primary">Add New Post</button>
+                <button class="btn btn-primary">Save Profile</button>
             </div>
         </div>
     </div>
